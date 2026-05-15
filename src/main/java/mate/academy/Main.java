b8ee65148cec244824918bfc7efb71558fce1f5e
Przemysław Ciżmiński <cizminski@interia.pl>
2026-05-15 21:10:38 +0200

    First try

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,55 +1,39 @@
 package mate.academy;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import mate.academy.model.CinemaHall;
-import mate.academy.model.Movie;
-import mate.academy.model.MovieSession;
-import mate.academy.service.CinemaHallService;
-import mate.academy.service.MovieService;
-import mate.academy.service.MovieSessionService;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.model.User;
+import mate.academy.security.AuthenticationService;
+import mate.academy.security.impl.AuthenticationServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        MovieService movieService = null;
+        AuthenticationService authenticationService = new AuthenticationServiceImpl();
 
-        Movie fastAndFurious = new Movie("Fast and Furious");
-        fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
-        movieService.add(fastAndFurious);
-        System.out.println(movieService.get(fastAndFurious.getId()));
-        movieService.getAll().forEach(System.out::println);
-
-        CinemaHall firstCinemaHall = new CinemaHall();
-        firstCinemaHall.setCapacity(100);
-        firstCinemaHall.setDescription("first hall with capacity 100");
-
-        CinemaHall secondCinemaHall = new CinemaHall();
-        secondCinemaHall.setCapacity(200);
-        secondCinemaHall.setDescription("second hall with capacity 200");
-
-        CinemaHallService cinemaHallService = null;
-        cinemaHallService.add(firstCinemaHall);
-        cinemaHallService.add(secondCinemaHall);
-
-        System.out.println(cinemaHallService.getAll());
-        System.out.println(cinemaHallService.get(firstCinemaHall.getId()));
-
-        MovieSession tomorrowMovieSession = new MovieSession();
-        tomorrowMovieSession.setCinemaHall(firstCinemaHall);
-        tomorrowMovieSession.setMovie(fastAndFurious);
-        tomorrowMovieSession.setShowTime(LocalDateTime.now().plusDays(1L));
-
-        MovieSession yesterdayMovieSession = new MovieSession();
-        yesterdayMovieSession.setCinemaHall(firstCinemaHall);
-        yesterdayMovieSession.setMovie(fastAndFurious);
-        yesterdayMovieSession.setShowTime(LocalDateTime.now().minusDays(1L));
-
-        MovieSessionService movieSessionService = null;
-        movieSessionService.add(tomorrowMovieSession);
-        movieSessionService.add(yesterdayMovieSession);
-
-        System.out.println(movieSessionService.get(yesterdayMovieSession.getId()));
-        System.out.println(movieSessionService.findAvailableSessions(
-                        fastAndFurious.getId(), LocalDate.now()));
+        try {
+            User firstUser = authenticationService.register("jacek@interia.pl", "");
+        } catch (RegistrationException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            User secondUser = authenticationService.register("marek@gmail", "HelloWorld");
+        } catch (RegistrationException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            User thirdUser = authenticationService.register("marek@gmail", "HelloWorld");
+        } catch (RegistrationException e) {
+            System.out.println(e.getMessage());;
+        }
+        try {
+            System.out.println(authenticationService.login("marek@gmail", ""));
+        } catch (AuthenticationException e) {
+            System.out.println(e.getMessage());;
+        }
+        try {
+            System.out.println(authenticationService.login("marek@gmail", "HelloWorld"));
+        } catch (AuthenticationException e) {
+            System.out.println(e.getMessage());;
+        }
     }
 }
---
+++ src/main/java/mate/academy/security/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,47 @@
+package mate.academy.security.impl;
+
+import java.util.Optional;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.model.User;
+import mate.academy.security.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.service.impl.UserServiceImpl;
+import mate.academy.util.HashUtil;
+
+public class AuthenticationServiceImpl implements AuthenticationService {
+
+    private UserService userService = new UserServiceImpl();
+
+    public AuthenticationServiceImpl() {
+    }
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> userFromObOptional = userService.findEmail(email);
+        if (userFromObOptional.isEmpty()) {
+            throw new AuthenticationException("Invalid email " + email);
+        }
+        User user = userFromObOptional.get();
+        String hashedPassword = HashUtil.hashPassword(password, user.getSalt());
+        if (hashedPassword.equals(user.getPassword())) {
+            return user;
+        }
+        throw new AuthenticationException("Invalid password");
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        if (password.isEmpty()) {
+            throw new RegistrationException("Password cannot be empty");
+        }
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        User addedUser = userService.add(user);
+        if (addedUser == null) {
+            throw new RegistrationException("Invalid email " + email);
+        }
+        return addedUser;
+    }
+}
